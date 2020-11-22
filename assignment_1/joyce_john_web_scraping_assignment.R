00e30b320decb0250838b2815322fef82dac89e2
John Joyce <Joyce_John@student.ceu.edu>
2020-11-22 18:10:38 +0100

    removed non-functional lines

--- assignment_1/joyce_john_web_scraping_assignment.R
+++ assignment_1/joyce_john_web_scraping_assignment.R
@@ -1,6 +1,7 @@
 ### Web Scraping Assignment
 
 #load the required libraries
+library(tidyverse)
 library(rvest)
 library(data.table)
 
@@ -13,53 +14,30 @@ t <- read_html(my_url)
 # write it out so I can inspect it and see if the relevant content is there
 write_html(t, 't.html')
 
-boxes <- 
-  t %>% 
-  html_nodes('h3')
-
-box_dfs <- data.frame()
-boxes[[1]]
-
+# make an index for dealing with duplicates
 keep_list <- seq(from = 1, to = 20, by = 2)
 
-# works but creates duplicates, could simply index with keep_list to remove them
-my_relative_link <- 
+# works but scrapes every title twice. use an index to deal with duplicates
+article_link <- 
   t %>% 
   html_nodes('.post-medium a')%>%
   html_attr('href')
 
 #works
-my_title <- t %>% html_nodes('.post-medium h3')%>%
+title <- t %>% 
+  html_nodes('.post-medium h3')%>%
   html_text()
 
 # works
-my_time <- t %>% html_nodes('.post-meta__option--datetime time') %>% html_text()
+time_published <- t %>% 
+  html_nodes('.post-meta__option--datetime time') %>% 
+  html_text()
 
 # works
-my_thumbnail <- t %>% 
+thumbnail_link <- t %>% 
   html_nodes('.size-archive-thumb')%>%
   html_attr('src')
 
 get_1_pentax_page <- function(my_url){
   
-  t <- read_html(my_url)
-  boxes <- 
-    t %>% 
-    html_nodes('.post-medium')
   
-  box_dfs <- lapply(boxes, function(x){
-    tlist <- list()
-    tlist[['my_title']] <- 
-      x %>% 
-      html_nodes('h3')%>%
-      html_text()
-    
-    my_relative_link <- 
-      x%>% 
-      html_nodes('post-medium__thumbnail')%>%
-      html_attr('href')
-
-    return(box_dfs)
-  })}
-
-df <- get_1_pentax_page(my_url)
